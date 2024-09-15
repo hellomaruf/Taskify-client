9a2ed67d3437aa5b6e66e74a774aca4aeda10368
maruf-ahmed <marufahamed1110@gmail.com>
2024-09-15 20:42:10 +0600

    Added create task functionality

--- src/Pages/CreateTask.jsx
+++ src/Pages/CreateTask.jsx
@@ -1,13 +1,35 @@
+import axios from "axios";
+
 function CreateTask() {
+  const handleSubmitTask = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const title = form.title.value;
+    const descreption = form.description.value;
+    const taskInfo = {
+      title,
+      descreption,
+    };
+    await axios
+      .post(`${import.meta.env.VITE_LOCALHOST_URL}/tasks`, taskInfo)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div>
       <div className="max-w-7xl mx-auto my-20 ">
         <div className="max-w-4xl mx-auto bg-[#F8F8FA] border p-6 rounded-xl">
           <div className=" mb-6 space-y-2">
             <h3 className="text-3xl font-semibold">Create a New Task</h3>
-            <p className="text-lg text-gray-500">Provide Information about the Task you Wish to Complete</p>
+            <p className="text-lg text-gray-500">
+              Provide Information about the Task you Wish to Complete
+            </p>
           </div>
-          <form  className="space-y-4">
+          <form onSubmit={handleSubmitTask} className="space-y-4">
             <div>
               <label className="sr-only" htmlFor="name">
                 Title
@@ -16,7 +38,8 @@ function CreateTask() {
                 className="w-full rounded-lg border-gray-200 p-3 text-sm"
                 placeholder="Task Title"
                 type="text"
-                id="name"
+                id="title"
+                name="title"
               />
             </div>
 
@@ -29,7 +52,7 @@ function CreateTask() {
                 className="w-full rounded-lg border-gray-200 p-3 text-sm"
                 placeholder="Description"
                 rows="8"
-                id="message"
+                name="description"
               ></textarea>
             </div>
 
